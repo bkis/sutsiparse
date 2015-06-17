5a0a55f50f7c349388e89a8fe0063cfdec31f6df
bkis <boerge.kiss@gmail.com>
2015-06-17 14:02:05 +0200

    parsing praparations (patterns etc)

--- SutsilvanStacks/src/de/spinfo/bkis/sutsiparse/Main.java
+++ SutsilvanStacks/src/de/spinfo/bkis/sutsiparse/Main.java
@@ -1,22 +1,35 @@
 package de.spinfo.bkis.sutsiparse;
 
+import java.io.IOException;
+
 import de.bkis.jaseval.JASEVAL;
 import de.bkis.jaseval.SVDocument;
 import de.spinfo.bkis.sutsiparse.io.IO;
 
 public class Main {
 	
-	/*		Ω	beginn Übersetzung
+	//PATTERNS
+	private static final String P_GENUS = 	"\\s[fmn]\\.(:|\\s\\([fmn]\\.\\))(?=\\s)";
+	private static final String P_GRAM = 	"(?<=\\s)(adj|adv|tr|int|tr\\/int|refl|pron|poss)\\.(?=\\s)"; //TODO: unvollst.
+	private static final String P_SEM = 	"(?<=\\s)\\((fig|bot|polit).?\\)(?=\\s)"; //TODO: unvollst.
+	private static final String P_SUBSEM = 	"(?<=\\s)\\([\\p{L}\\s]+\\)(?=\\s)"; //TODO: ?
+	
+	/*		
+	 * 		MARKERS:
+	 * 		Ω	beginn Übersetzung
 	 * 		^	Ende des Eintrags
 	 * 		≤	Wort hier nochmal einfügen
 	 * 		%	andere (eigene) Schreibweise
-	 * 		¡	i mit Punkt drunter
-	 * 		
-	 * 		
-	 * 		
-	 * 		
-	 * 		
-	 * 		
+	 * 
+	 * 		REPLACEMENTS:
+	 * 		”	-
+	 * 		å	a
+	 * 		™	e
+	 * 		¡	i
+	 * 		ø	o
+	 * 		°	u
+	 * 		&lt;[0-9]&gt;	entfernen
+	 * 		\\[[0-9]\\]		entfernen
 	 * 		
 	 */
 
@@ -27,6 +40,29 @@ public class Main {
 		// extrahierte hypercard-daten (nur reintext)
 		//in eine gemeinsame datei schreiben
 		//io.convertToTxt("datenXML", "sutsilvan_data.txt");
+		
+		
+//		// CLEANUP
+//		String[] entries = io.readEntryLines("sutsilvan_data.txt");
+//		StringBuilder sb = new StringBuilder();
+//		for (String line : entries){
+//			line = line.replaceAll("”", "-");
+//			line = line.replaceAll("å", "a");
+//			line = line.replaceAll("™", "e");
+//			line = line.replaceAll("¡", "i");
+//			line = line.replaceAll("ø", "o");
+//			line = line.replaceAll("°", "u");
+//			line = line.replaceAll("&lt;[0-9]&gt;", "");
+//			line = line.replaceAll("\\[[0-9]\\]\\s", "");
+//			sb.append(line + "\n");
+//		}
+//		try {
+//			jaseval.writeToFile(sb.toString(), "sutsilvan_data_clean.txt", "UTF-8");
+//		} catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+		
 		
 		SVDocument doc = new SVDocument();
 		String[] fields = {"RecID", "DStichwort", "DGenus", "DGrammatik", "RStichwort",
@@ -39,8 +75,6 @@ public class Main {
 				"XTempStichwort", "XTempGrammatik", "varCopiar", "Remartgas", "maalr_timestamp",
 				"Bearbeitungshinweis2", "xvarMail", "xDiever", "vVersiun", "redirect_a", "redirect_b"};
 		for (String f : fields) doc.addField(f);
-		
-		
 		
 		String[] entries = io.readEntryLines("sutsilvan_data.txt");
 		
